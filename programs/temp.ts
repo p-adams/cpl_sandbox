5232558e03034ee53f9427317d04717dc7f8fe31
Paul Adams <ghusiyy@gmail.com>
2022-05-06 01:53:43 -0700

    make print table accept table type

--- programs/temp.ts
+++ programs/temp.ts
@@ -2,17 +2,25 @@ export function TempConverter() {
   const toC = (temp: number): number => Math.round((temp - 32) / 1.8);
   const toF = (temp: number): number => Math.round(temp * 1.8 + 32);
   return {
+    // export for test
     toC,
     toF,
-    printToCTable: (header: string, step: number, limit: number) => {
+    printTable: (
+      header: string,
+      step: number,
+      limit: number,
+      tableType: "F" | "C"
+    ) => {
       console.log(header);
       let i;
       i = 0;
+      const tableStr = (t: number) =>
+        tableType === "F" ? `F: ${t} = C: ${toC(t)}` : `C: ${t} = F: ${toF(t)}`;
       for (; i <= limit; i += step) {
-        console.log(`F: ${i} = C: ${toC(i)}`);
+        console.log(tableStr(i));
       }
     },
   };
 }
 
-console.log(TempConverter().printToCTable("f to c", 20, 300));
+console.log(TempConverter().printTable("f to c", 20, 300, "F"));
