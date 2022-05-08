16f8bb3dffece96d6f3e66dea18485da76c68e7f
Paul Adams <ghusiyy@gmail.com>
2022-05-07 23:57:26 -0700

    extract prompt function

--- programs/io.ts
+++ programs/io.ts
@@ -1,16 +1,16 @@
 import { readLines } from "https://deno.land/std@0.138.0/io/buffer.ts";
 export async function echoInput() {
   while (true) {
-    const input = await pprompt();
+    const input = await getInput();
     if (input === undefined) {
       return false;
     }
     console.log(input);
   }
+}
 
-  async function pprompt() {
-    for await (const input of readLines(Deno.stdin)) {
-      return input;
-    }
+async function getInput() {
+  for await (const input of readLines(Deno.stdin)) {
+    return input;
   }
 }
